8c4d37f5734ad8af43f67d79a40dda87ccab0052
Pawan-Kumar-Singh-284 <19krpawan@gmail.com>
2024-02-17 20:02:41 +0530

    add api

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+  params: { hl: "en", gl: "US" },
+  headers: {
+    "X-RapidAPI-Key": "0dfb99fbadmsh113b30e3e2ab33fp1d291cjsnd6771a8728d6",
+    "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+  },
+};
+
+export const fetchDataFromApi = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
